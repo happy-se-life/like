ded48da1b2ccb0e5d577b3cf04ab19729dcc7c06
happy-se-life <kohei_nom@yahoo.co.jp>
2020-02-22 17:00:25 +0900

    Allows you to select an icon from 6 designs.

---
+++ app/models/like_constants.rb
@@ -0,0 +1,15 @@
+#
+# Constant definition
+#
+class LikeConstants
+    #
+    # Choose an icon design
+    # "icon_heart"
+    # "icon_heart_shadow"
+    # "icon_star"
+    # "icon_star_shadow"
+    # "icon_thumbsup"
+    # "icon_thumbsup_shadow"
+    #
+    ICON_DESIGN = "icon_heart"
+end
--- lib/hooks.rb
+++ lib/hooks.rb
@@ -3,17 +3,19 @@ module LikeHooks
     # Like icon on issue
     def view_issues_show_details_bottom(context={})
       like_count = Like.where(like_id: context[:issue].id).where(like_type: 'issue').length
+      icon_design = LikeConstants::ICON_DESIGN
       context[:controller].send(:render_to_string, {
         :partial => "issues/issuesShowDetailsBottom",
-        :locals => { id: context[:issue].id, count: like_count, type: 'issue' }
+        :locals => { id: context[:issue].id, count: like_count, type: 'issue', design: icon_design}
       })
     end
     # Like icon on Journal
     def view_issues_history_journal_bottom(context={})
           like_count = Like.where(like_id: context[:journal].id).where(like_type: 'journal').length
+          icon_design = LikeConstants::ICON_DESIGN
           context[:controller].send(:render_to_string, {
             :partial => "issues/issuesHistoryJournalBottom",
-            :locals => { id: context[:journal].id, count: like_count, type: 'journal' }
+            :locals => { id: context[:journal].id, count: like_count, type: 'journal', design: icon_design}
           })
     end
     # Like icon on Wiki
@@ -36,9 +38,10 @@ module LikeHooks
         like_count = 0
         is_wiki = false
       end
+      icon_design = LikeConstants::ICON_DESIGN
       context[:controller].send(:render_to_string, {
         :partial => "wiki/layoutsBaseContent",
-        :locals => { id: wiki_id, count: like_count, type: 'wiki', is_wiki: is_wiki}
+        :locals => { id: wiki_id, count: like_count, type: 'wiki', is_wiki: is_wiki, design: icon_design}
       })
     end
   end
